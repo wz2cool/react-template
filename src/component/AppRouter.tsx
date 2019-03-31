d5762fdee849091a0cd9f434552f84960f6e35b4
wz2cool <wz2cool@live.cn>
2019-04-01 00:34:28 +0800

    add

--- src/component/AppRouter.tsx
+++ src/component/AppRouter.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route, HashRouter, Switch, Link } from "react-router-dom";
+import { Route, HashRouter, Link } from "react-router-dom";
 import Home from "../module/dashboard/Home";
 
 const About = React.lazy(() => import("../module/dashboard/About"));
@@ -12,11 +12,11 @@ export class AppRouter extends React.PureComponent {
         <Link to="/">Home</Link>
         <Link to="/about">about</Link>
         <Link to="/readme">readme</Link>
-        <Switch>
+        <React.Suspense fallback={<div>Loading...</div>}>
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/readme" component={Readme} />
-        </Switch>
+        </React.Suspense>
       </HashRouter>
     );
   }
