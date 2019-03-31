b5163ceec250eb8b11e85cb8aaf86a756eb617f2
wz2cool <wz2cool@live.cn>
2019-03-31 17:56:08 +0800

    add hot loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
 
-export class App extends React.Component {
-  public render() {
-    return hot(<div>Hello React!</div>);
-  }
-}
+const App = () => <div>Hello World!</div>;
+
+export default hot(App);
